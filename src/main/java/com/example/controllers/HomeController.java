b4e076798934699f3937e67a9a15fb80aa996f85
Matthias Radin <matthiasradin7@gmail.com>
2024-11-22 16:55:24 +0100

    Merge main

--- src/main/java/com/example/controllers/HomeController.java
+++ src/main/java/com/example/controllers/HomeController.java
@@ -15,10 +15,64 @@ public class HomeController {
 
         if (authentication != null && authentication.isAuthenticated() &&
             !"anonymousUser".equals(authentication.getPrincipal())) {
-           
             model.addAttribute("username", authentication.getName());
         }
-
         return "home"; 
     }
+
+
+    @GetMapping("/admin")
+    public String showAdminPage() {
+        return "admin"; 
+    }
+
+    @GetMapping("/article1")
+    public String showArticle1Page() {
+        return "article1"; 
+    }
+
+    @GetMapping("/article2")
+    public String showArticle2Page() {
+        return "article2"; 
+    }
+
+    @GetMapping("/article3")
+    public String showArticle3Page() {
+        return "article3"; 
+    }
+
+    @GetMapping("/game")
+    public String showGamePage() {
+        return "game"; 
+    }
+
+    @GetMapping("/logique")
+    public String showLogiquePage() {
+        return "logique"; 
+    }
+
+    @GetMapping("/maths")
+    public String showMathsPage() {
+        return "maths"; 
+    }
+
+    @GetMapping("/crypto")
+    public String showCryptoPage() {
+        return "crypto"; 
+    }
+
+    @GetMapping("/sous")
+    public String showSousPage() {
+        return "sous"; 
+    }
+
+    @GetMapping("/leaderboard")
+    public String showLeaderboardPage() {
+        return "leaderboard"; 
+    }
+
+
+
+
+
 }
