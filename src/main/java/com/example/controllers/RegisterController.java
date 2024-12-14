39520d4d0e49bc18ba1685f1980cde07d5b2b67b
gwennbrln <bgwenn33@gmail.com>
2024-12-14 15:14:31 +0100

    commit header

--- src/main/java/com/example/controllers/RegisterController.java
+++ src/main/java/com/example/controllers/RegisterController.java
@@ -1,14 +1,18 @@
 package com.example.controllers;
 
-import com.example.entities.Utilisateur;
-import com.example.repository.UtilisateurRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.entities.Utilisateur;
+import com.example.repository.UtilisateurRepository;
+
+import org.springframework.ui.Model;
+
+
 
 @Controller
 public class RegisterController {
@@ -16,36 +20,34 @@ public class RegisterController {
     @Autowired
     private UtilisateurRepository utilisateurRepository;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;  // Utilisation de PasswordEncoder
-
     @GetMapping("/register")
     public String showRegisterPage() {
-        return "register";  
+        return "register";
     }
 
+
     @PostMapping("/register")
-    public String registerUser(@RequestParam String nom,
+    public String registerUser(@RequestParam String nom ,
                                @RequestParam String email, 
                                @RequestParam String motDePasse, 
                                @RequestParam String confirmMotDePasse, 
                                Model model) {
 
-        // Vérifier si les mots de passe correspondent
+
         if (!motDePasse.equals(confirmMotDePasse)) {
             model.addAttribute("error", "Les mots de passe ne correspondent pas.");
             return "register";
         }
 
-        // Hacher le mot de passe avec le PasswordEncoder configuré
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         String hashedPassword = passwordEncoder.encode(motDePasse);
 
-        // Créer l'utilisateur avec 1000 sous par défaut
+
         Utilisateur utilisateur = new Utilisateur();
-        utilisateur.setNom(nom);
+        utilisateur.setNom(motDePasse);
         utilisateur.setEmail(email);
-        utilisateur.setMotDePasse(hashedPassword);
-        utilisateur.setSous(1000);
+        utilisateur.setMotDePasse(hashedPassword); 
+
 
         utilisateurRepository.save(utilisateur);
 
